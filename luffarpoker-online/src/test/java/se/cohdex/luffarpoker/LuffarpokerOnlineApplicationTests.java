b39f7c4b6413195d9ec07979acd6a8e80137fd76
Jonathan Cohlin <jonathan.cohlin@gmail.com>
2020-01-05 14:45:30 +0100

    initial project setup

---
+++ luffarpoker-online/src/test/java/se/cohdex/luffarpoker/LuffarpokerOnlineApplicationTests.java
@@ -0,0 +1,12 @@
+package se.cohdex.luffarpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuffarpokerOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
